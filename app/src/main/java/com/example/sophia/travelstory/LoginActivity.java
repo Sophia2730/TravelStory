b1d452510453f7ff0b487655a9fc2dec4379b53b
ChanSu <sophia@ChanSuui-MacBook-Pro.local>
2017-05-19 13:18:15 +0900

    로그인 화면 xml 구성

---
+++ app/src/main/java/com/example/sophia/travelstory/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.sophia.travelstory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btn_register = (Button) findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
